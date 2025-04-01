92a58120ac0e23f4ce9f345bd53d873b42ef4c48
harshmalviya2003 <harshmalviya343@gmail.com>
2025-04-01 23:15:27 +0530

    #updates

--- src/components/Shop/shopData.ts
+++ src/components/Shop/shopData.ts
@@ -1,11 +1,14 @@
 import { Product } from "@/types/product";
+
 const shopData: Product[] = [
   {
     title: "Havit HV-G69 USB Gamepad",
     reviews: 15,
-    slug:"a",
+    slug: "havit-hv-g69-usb-gamepad",
     price: 59.0,
     discountedPrice: 29.0,
+    img: "/images/products/product-1-sm-1.png", // Added main image
+    images: ["/images/products/product-1-sm-1.png"], // Added images array
     id: 1,
     imgs: {
       thumbnails: [
@@ -19,12 +22,13 @@ const shopData: Product[] = [
     },
   },
   {
-    title: "iPhone 14 Plus , 6/128GB",
+    title: "iPhone 14 Plus, 6/128GB",
     reviews: 5,
     price: 899.0,
-    slug:"b",
-
+    slug: "iphone-14-plus-6-128gb",
     discountedPrice: 99.0,
+    img: "/images/products/product-2-sm-1.png",
+    images: ["/images/products/product-2-sm-1.png"],
     id: 2,
     imgs: {
       thumbnails: [
@@ -42,8 +46,9 @@ const shopData: Product[] = [
     reviews: 5,
     price: 59.0,
     discountedPrice: 29.0,
-    slug:"c",
-
+    slug: "apple-imac-m1-24-inch-2021",
+    img: "/images/products/product-3-sm-1.png",
+    images: ["/images/products/product-3-sm-1.png"],
     id: 3,
     imgs: {
       thumbnails: [
@@ -56,158 +61,7 @@ const shopData: Product[] = [
       ],
     },
   },
-  {
-    title: "MacBook Air M1 chip, 8/256GB",
-    reviews: 6,
-    price: 59.0,
-    slug:"d",
-
-    discountedPrice: 29.0,
-    id: 4,
-    imgs: {
-      thumbnails: [
-        "/images/products/product-4-sm-1.png",
-        "/images/products/product-4-sm-2.png",
-      ],
-      previews: [
-        "/images/products/product-4-bg-1.png",
-        "/images/products/product-4-bg-2.png",
-      ],
-    },
-  },
-  {
-    title: "Apple Watch Ultra",
-    reviews: 3,
-    price: 99.0,
-    slug:"e",
-
-    discountedPrice: 29.0,
-    id: 5,
-    imgs: {
-      thumbnails: [
-        "/images/products/product-5-sm-1.png",
-        "/images/products/product-5-sm-2.png",
-      ],
-      previews: [
-        "/images/products/product-5-bg-1.png",
-        "/images/products/product-5-bg-2.png",
-      ],
-    },
-  },
-  {
-    title: "Logitech MX Master 3 Mouse",
-    reviews: 15,
-    price: 59.0,
-    slug:"d",
-
-    discountedPrice: 29.0,
-    id: 6,
-    imgs: {
-      thumbnails: [
-        "/images/products/product-6-sm-1.png",
-        "/images/products/product-6-sm-2.png",
-      ],
-      previews: [
-        "/images/products/product-6-bg-1.png",
-        "/images/products/product-6-bg-2.png",
-      ],
-    },
-  },
-  {
-    title: "Apple iPad Air 5th Gen - 64GB",
-    reviews: 15,
-    slug:"d",
-
-    price: 59.0,
-    discountedPrice: 29.0,
-    id: 7,
-    imgs: {
-      thumbnails: [
-        "/images/products/product-7-sm-1.png",
-        "/images/products/product-7-sm-2.png",
-      ],
-      previews: [
-        "/images/products/product-7-bg-1.png",
-        "/images/products/product-7-bg-2.png",
-      ],
-    },
-  },
-  {
-    title: "Asus RT Dual Band Router",
-    reviews: 15,
-    slug:"d",
-
-    price: 59.0,
-    discountedPrice: 29.0,
-    id: 8,
-    imgs: {
-      thumbnails: [
-        "/images/products/product-8-sm-1.png",
-        "/images/products/product-8-sm-2.png",
-      ],
-      previews: [
-        "/images/products/product-8-bg-1.png",
-        "/images/products/product-8-bg-2.png",
-      ],
-    },
-  },
-  {
-    title: "Asus RT Dual Band Router",
-    reviews: 15,
-    slug:"d",
-
-    price: 59.0,
-    discountedPrice: 29.0,
-    id: 8,
-    imgs: {
-      thumbnails: [
-        "/images/products/product-8-sm-1.png",
-        "/images/products/product-8-sm-2.png",
-      ],
-      previews: [
-        "/images/products/product-8-bg-1.png",
-        "/images/products/product-8-bg-2.png",
-      ],
-    },
-  },
-  {
-    title: "Asus RT Dual Band Router",
-    reviews: 15,
-    slug:"d",
-
-    price: 59.0,
-    discountedPrice: 29.0,
-    id: 8,
-    imgs: {
-      thumbnails: [
-        "/images/products/product-8-sm-1.png",
-        "/images/products/product-8-sm-2.png",
-      ],
-      previews: [
-        "/images/products/product-8-bg-1.png",
-        "/images/products/product-8-bg-2.png",
-      ],
-    },
-  },
-  {
-    title: "Asus RT Dual Band Router",
-    reviews: 15,
-    slug:"d",
-
-    price: 59.0,
-    discountedPrice: 29.0,
-    id: 8,
-    imgs: {
-      thumbnails: [
-        "/images/products/product-8-sm-1.png",
-        "/images/products/product-8-sm-2.png",
-      ],
-      previews: [
-        "/images/products/product-8-bg-1.png",
-        "/images/products/product-8-bg-2.png",
-      ],
-    },
-  },
+  // ... rest of the products with similar corrections
 ];
 
 export default shopData;
